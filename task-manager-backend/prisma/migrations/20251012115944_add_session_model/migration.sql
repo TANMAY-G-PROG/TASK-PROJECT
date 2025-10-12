aa7678320945ccf7ad5180d3044d9ca85a6a1262
Tanmay G Shetty <tanmay.121cr7@gmail.com>
2025-10-12 17:35:47 +0530

    Auth changes for github

---
+++ task-manager-backend/prisma/migrations/20251012115944_add_session_model/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "public"."Session" (
+    "id" SERIAL NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "public"."Session"("sessionToken");
+
+-- CreateIndex
+CREATE INDEX "Session_userId_idx" ON "public"."Session"("userId");
+
+-- CreateIndex
+CREATE INDEX "Session_sessionToken_idx" ON "public"."Session"("sessionToken");
+
+-- AddForeignKey
+ALTER TABLE "public"."Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
